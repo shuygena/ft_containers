facb2d0057ef0b11b518ab7e429699ff413e2a72
shuygena <i.am.zuzya@gmail.com>
2022-07-26 03:19:10 +0300

    utils: add ft::pair struct

--- headers/utils.hpp
+++ headers/utils.hpp
@@ -62,7 +62,32 @@ namespace ft {
         for (; first1 != last1 && first2 != last2 && !(*first1 < *first2) && !(*first2 < *first1);
         ++first1, ++first2);
         return first2 == last2 ? false : first1 == last1 || *first1 < *first2;
+        };
+
+    template <class T1, class T2>
+        struct pair {
+            typedef T1 first_type;
+            typedef T2 second_type;
+            T1 first;
+            T2 second;
+            pair() : first(T1()), second(T2()) {}
+            pair(const T1& x, const T2& y): first(x), second(y){}
+            template<class U, class V>
+                pair(const pair<U, V> &p): first(p.first), second(p.second) {}
+            };
+
+            template <class T1, class T2>
+                bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y){
+                    return x.first == y.first && x.second == y.second;
+                }
+            template <class T1, class T2>
+                bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y){
+                    return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
+                    }
+            template <class T1, class T2>
+                pair<T1, T2> make_pair(const T1& x, const T2& y){
+                    return pair<T1, T2>(x, y);
+                    }
         }
-}
 
 #endif
