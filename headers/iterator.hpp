0c10e6545dfb710b6c956609fe314030aea441b6
shuygena <i.am.zuzya@gmail.com>
2022-08-14 22:48:13 +0300

    iterator: clear code

--- headers/iterator.hpp
+++ headers/iterator.hpp
@@ -3,17 +3,6 @@
 
 
 namespace ft {
-    // template<class Category, class T, class Distance = ptrdiff_t,
-    // class Pointer = T*, class Reference = T&>
-    // struct iterator {
-    //     typedef T value_type;
-    //     typedef Distance difference_type;
-    //     typedef Pointer pointer;
-    //     typedef Reference reference;
-    //     typedef Category iterator_category;
-    // };
-
-    
     template<class Iterator>
     struct iterator_traits {
     typedef typename Iterator::difference_type difference_type;
